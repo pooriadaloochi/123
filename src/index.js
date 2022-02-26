2140ec548e3d4db424c25ec9f5060c72ee7639b9
poori <74615252+pooriadaloochi@users.noreply.github.com>
2022-02-26 15:32:57 +0330

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const app = require("express")();
+const http = require("http").createServer(app);
+
+const io = require("socket.io")(http);
+
+var players = {};
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log(`User ${socket.id} connected`);
+  players[socket.id] = { id: socket.id, object: {} };
+
+  socket.on("disconnect", (reason) => {
+    console.log(`User ${socket.id} disconnected`);
+    delete players[socket.id];
+  });
+
+  // Main script //
+  socket.on("senddata", (data) => {
+    players[socket.id].object = data;
+  });
+});
+
+setInterval(() => {
+  io.emit("playerdata", players);
+}, 10);
+
+http.listen(8080, () => {
+  console.log("listening on *:8080");
+});
